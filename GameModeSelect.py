c357e3b4976929f10758365c5660017c683324de
raymond-blok <raymond-blok@outlook.com>
2020-01-31 09:59:34 +0000

    gameModeSelect max added better

--- GameModeSelect.py
+++ GameModeSelect.py
@@ -5,19 +5,19 @@ class GameModeSelect:
         self.GameModes.append({"name": "normal"})
         self.selectedMode = [1,1]
         self.currentField = 0
-        self.max = [4, 4]
+        self.max = [4, 3]
         self.min = [1, 1]
             # Initialize a list of GameModes
 
             # list of current hardware components.
     # function to add an amount to field.
     def incrementField(self):
-        if(self.max[self.currentField]):
+        if(self.max[self.currentField] > self.selectedMode[self.currentField]):
             print("increment")
             self.selectedMode[self.currentField] = self.selectedMode[self.currentField] + 1
         # function to decrease amount to field.
     def decrementField(self):
-        if(self.min[self.currentField]):
+        if(self.min[self.currentField] < self.selectedMode[self.currentField]):
             print("decrement")
             self.selectedMode[self.currentField] = self.selectedMode[self.currentField] - 1
 
